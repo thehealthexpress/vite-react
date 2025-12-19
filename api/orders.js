51bf75fecbf1c6c5ba259cd92734cfce85636e51
thehealthexpress <thehealthexpress123@gmail.com>
2025-12-19 09:33:40 +0530

    Update orders.js

--- api/orders.js
+++ api/orders.js
@@ -1,10 +1,3 @@
-let orders = global.orders || [];
-global.orders = orders;
-
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    return res.json(orders);
-  }
-
-  res.status(405).json({ error: "Method not allowed" });
+  res.status(200).json([]);
 }
