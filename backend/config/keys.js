76d77306b093bff4926ccb4edc1f6420d47eb5f0
thehealthexpress <thehealthexpress123@gmail.com>
2026-02-16 20:02:28 +0530

    Delete backend directory

--- backend/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  MONGO_URI: process.env.MONGO_URI || 'your_mongodb_connection_string_here'
-};
--- backend/routes/orderRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createOrder } = require("../controllers/orderController");
-
-router.post("/", createOrder);
-
-module.exports = router;
