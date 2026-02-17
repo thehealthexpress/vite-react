04864821be71e1c07eb5eb66c123f07e665c5e29
thehealthexpress <thehealthexpress123@gmail.com>
2026-02-17 10:41:23 +0530

    Add authentication routes for registration and login

---
+++ server/routes/auth.js
@@ -0,0 +1,25 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = await User.create({ name, email, password: hashed });
+  res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const valid = await bcrypt.compare(password, user.password);
+  if (!valid) return res.status(400).json("Invalid credentials");
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token });
+});
+
+export default router;
