17698041a72fa54a33f8c30548f610f6b86b70cc
thehealthexpress <thehealthexpress123@gmail.com>
2026-02-17 10:12:23 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import AddressModal from "./AddressModal";
 
 export default function Navbar() {
+  const [show, setShow] = useState(false);
+  const address = localStorage.getItem("address");
+
   return (
     <div className="navbar">
-      <h2>THE HEALTH EXPRESS</h2>
       <div>
+        <h2>THE HEALTH EXPRESS</h2>
+        <p onClick={() => setShow(true)} className="address">
+          {address ? address : "Use Your Current Location"}
+        </p>
+      </div>
+
+      <div className="nav-links">
         <Link to="/">Home</Link>
         <Link to="/menu">Menu</Link>
         <Link to="/orders">My Orders</Link>
       </div>
+
+      {show && <AddressModal close={() => setShow(false)} />}
     </div>
   );
 }
