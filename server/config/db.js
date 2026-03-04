8511459f89cfc7856027185cd4f1ae8b741faef0
thehealthexpress <thehealthexpress123@gmail.com>
2026-03-04 19:24:15 +0530

    Delete server directory

--- server/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/thehealthexpress");
-  console.log("MongoDB Connected");
-};
-
-export default connectDB;
